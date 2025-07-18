27dda5d0dcbe5356adceacd7be786c878b08876e
Breno Nascimento <satsukebare23@gmail.com>
2025-07-18 11:41:53 -0300

    add keys

--- src/components/ManageStudents.tsx
+++ src/components/ManageStudents.tsx
@@ -244,12 +244,10 @@ export default function ManageStudents() {
                     navigate("/chaves");
                   }}
                   className="w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 disabled:opacity-50 disabled:cursor-not-allowed transition-colors cursor-pointer mb-2 cursor-pointer mb-6"
-                  disabled
                 >
                   <span className="text-center">
                     VER CHAVEAMENTO DE ATLETAS
                   </span>
-                  <span>(Sendo montado)</span>
                 </button>
                 <div className="space-y-4">
                   {filteredStudents?.length === 0 ? (
